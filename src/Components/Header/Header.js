41c09d9f70ba08a9373fa4bff45fd2ad2f84ff5d
tanvirhossains <tanvirhossaincou2020@gmail.com>
2022-04-05 19:43:18 +0600

    home page added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
+
 
 const Header = () => {
     return (
 
 
-        <div>
-            <Link to="/home">Home</Link>
-            <Link to="/reviews">Reviews</Link>
-            <Link to='/dashboard'>Dash Board</Link>
-            <Link to='/blogs'>Blogs</Link>
-            <Link to='/about'>About</Link>
+        <div className='link'>
+            <Link className='Link'  to="/home">Home</Link>
+            <Link className='Link' to="/reviews">Reviews</Link>
+            <Link className='Link' to='/dashboard'>Dash Board</Link>
+            <Link className='Link' to='/blogs'>Blogs</Link>
+            <Link className='Link' to='/about'>About</Link>
 
            
         </div>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
-           <h1>Well come to the </h1>
+            <h1>Well come to the product home page!!! </h1>
+
+            <div className=' bike-info'>
+                <img src="https://static.autox.com/uploads/bikes/2021/08/indian-chief-bobber-dark-horse.jpg" alt="" />
+                
+                <div className='bike-detail'>
+                    <h1><span className='span1'>  Best bike forever </span><br /> <span className='span2'>For You</span></h1>
+
+                    <p>Honda Hornet 2.0 is a street bike available at a starting price of Rs. 1,33,521 in India. It is available in only 1 variant and 5 colours. The Honda Hornet 2.0 is powered by 184.4cc BS6 engine which develops a power of 17.03 bhp and a torque of 16.1 Nm. </p>
+
+                    <button>Buy Now</button>
+                </div>
+
+            </div>
         </div>
     );
 };
