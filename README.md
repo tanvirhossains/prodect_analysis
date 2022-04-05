1252a7cd4f73a3662edd9e4fb0cc22de394e0a7e
tanvirhossains <tanvirhossaincou2020@gmail.com>
2022-04-05 22:43:58 +0600

    readme.me editing

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Getting Started with Create React App
 
+## prject overview:- 
++ Reviwing the bike on the home page with 3 questins 
++ 
+
+
+
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
 
             <div className=' bike-info'>
                 <img src="https://static.autox.com/uploads/bikes/2021/08/indian-chief-bobber-dark-horse.jpg" alt="" />
-                
+
                 <div className='bike-detail'>
                     <h1><span className='span1'>  Best bike forever </span><br /> <span className='span2'>For You</span></h1>
 
@@ -18,9 +18,9 @@ const Home = () => {
                     <button>Buy Now</button>
                 </div>
 
-                
+
             </div>
-            
+
             <h2 >Customer Reviews (3) </h2>
 
 
@@ -28,6 +28,37 @@ const Home = () => {
 
 
             <Link className='all-review-button' to="/reviews"> See All Reviws</Link>
+
+
+            <div className='qus-ans-div'>
+                <div>
+                    <h3>What is semantic tag </h3>
+                    <p>
+                        1.Semantic tag are those user use to describe the element clearly to the machine. <br />
+                        2.This tag halps the  user to understand the importance of the ta <br />
+                        3. this tag make easier to understand to the user and reader also <br />
+                        4. It has greater accessibility as well as offer to the user better experience also. <br />
+                        5.Example of semantic tag are: - form, article , table , footer , header, nav ,time etc
+                    </p>
+                </div>
+                <div>
+                    <h3>what is context api? </h3>
+                    <p>
+                        1. context api is new or update and easy way to "props drilling" <br />
+                        2. context api way to globally variable passing system so that a uses can use the data from anywhere <br />
+                        3. It is a feature of react api to make easy the collecting data <br />
+                    </p>
+                </div>
+                <div>
+                    <h3> Inline vs Block</h3>
+                    <p>
+                        1.In block height and width can increase and decrease but in Inline couldn't <br />
+                        2. In Inline margin can be given but in Block not. <br />
+                        3.
+                    </p>
+                </div>
+            </div>
+
         </div>
     );
 };
